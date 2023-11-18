ea4086978a25358652bea517a18a4c6af8b3f44a
bresha <hrvoje.bresic@gmail.com>
2023-11-18 14:31:36 +0100

    added sim db

--- githubtrackerapi/schemas.py
+++ githubtrackerapi/schemas.py
@@ -1,8 +1,13 @@
 from pydantic import BaseModel
 from datetime import datetime
-
+import json
 
 class GithubMember(BaseModel):
     id: int
     handler: str
     date_added: datetime | None
+
+
+def load_db() -> list[GithubMember]:
+    with open("githubmembers.json") as f:
+        return [GithubMember.parse_obj(obj) for obj in json.load(f)]
